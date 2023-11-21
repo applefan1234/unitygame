b31eaffdb7f839812e86e6179dd1c581bce6a49f
applefan1234 <ps.ukfash@gmail.com>
2023-11-21 12:58:50 +0100

    fixed bug

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -1,28 +1,93 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
-public class movement : MonoBehaviour
+public class CharacterMoveController : MonoBehaviour
 {
-    public float speed = 3;
-    private Rigidbody2D rb;
 
-    public float Move;
-    
-        // Start is called before the first frame update
-    void Start()
+
+
+
+
+    [SerializeField] private float jumpHeight;
+    [SerializeField] private float moveSpeed = 140f;
+    private float _moveDir = 1;
+
+    private Rigidbody2D _rigidbody2D;
+    private Vector3 _moveVel;
+    public float jumpAmount = 10;
+    public float gravityScale = 10;
+    public float fallingGravityScale = 40;
+    bool onground;
+
+
+    void Move()
     {
-        Rigidbody rb = GetComponent<Rigidbody>();
-          
+        _moveVel = _rigidbody2D.velocity;
+        _moveVel.x = _moveDir * moveSpeed * Time.fixedDeltaTime;
+        _rigidbody2D.velocity = _moveVel;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Check if the collision is with a ground object (you may need to set up a "Ground" layer)
+        if (collision.gameObject.tag == "ground")
+        {
+            onground = true;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-       
+
+
+
+    private void Awake()
     {
-        //Move = 
-        if (Input.GetKey("a")){
-            Debug.Log(" input resived");
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        Move();
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("button is pressed");
+
         }
+
+    }
+
+    private void Update()
+    {
+
+
+        if (Input.GetButtonDown("Fire1") && onground == true)
+        {
+            _rigidbody2D.AddForce(Vector2.up * jumpAmount, ForceMode2D.Impulse);
+            onground = false;
+        }
+        if (_rigidbody2D.velocity.y >= 0)
+        {
+            _rigidbody2D.gravityScale = gravityScale;
+        }
+        else if (_rigidbody2D.velocity.y < 0)
+        {
+            _rigidbody2D.gravityScale = fallingGravityScale;
+            GetInput();
+        }
+
+
+        
+
+
+
+
+        void GetInput()
+        {
+
+        }
+
+
     }
 }
